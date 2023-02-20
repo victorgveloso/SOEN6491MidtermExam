46b292580769e063ee9cb4281e5b5440a7b5d8ae
Victor Guerra Veloso <victorgvbh@gmail.com>
2023-02-20 13:54:23 -0500

    Extract constant AFTER_LIMIT_RATE = 0.062 (Fix #3)

--- src/main/java/DisabilitySite.java
+++ src/main/java/DisabilitySite.java
@@ -2,6 +2,7 @@ import java.util.Date;
 
 public class DisabilitySite extends AbstractSite {
 	private static final Dollars FUEL_TAX_CAP = new Dollars (0.10);
+	public static final double AFTER_LIMIT_RATE = 0.062;
 	private Zone _zone;
 	private static final int CAP = 200;
 
@@ -14,7 +15,7 @@ public class DisabilitySite extends AbstractSite {
 		Dollars result;
 		int usage = Math.min(fullUsage, CAP);
 		result = _zone.computeSeasonRate(usage, start, end);
-		result = result.plus(new Dollars (Math.max(fullUsage - usage, 0) * 0.062));
+		result = result.plus(new Dollars (Math.max(fullUsage - usage, 0) * AFTER_LIMIT_RATE));
 		result = result.plus(result.times(TAX_RATE));
 		Dollars fuel = new Dollars(fullUsage * FUEL_RATE);
 		result = result.plus(fuel);
