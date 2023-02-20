3a083f3a44cbcf15c7c20f242e85af71f8891e64
Victor Guerra Veloso <victorgvbh@gmail.com>
2023-02-20 12:42:58 -0500

    Extract constant FUEL_RATE = 0.0175 (#3)

--- src/main/java/DisabilitySite.java
+++ src/main/java/DisabilitySite.java
@@ -2,6 +2,7 @@ import java.util.Date;
 
 public class DisabilitySite extends AbstractSite {
 	private static final Dollars FUEL_TAX_CAP = new Dollars (0.10);
+	public static final double FUEL_RATE = 0.0175;
 	private Zone _zone;
 	private static final int CAP = 200;
 
@@ -16,7 +17,7 @@ public class DisabilitySite extends AbstractSite {
 		result = _zone.computeSeasonRate(usage, start, end);
 		result = result.plus(new Dollars (Math.max(fullUsage - usage, 0) * 0.062));
 		result = result.plus(result.times(TAX_RATE));
-		Dollars fuel = new Dollars(fullUsage * 0.0175);
+		Dollars fuel = new Dollars(fullUsage * FUEL_RATE);
 		result = result.plus(fuel);
 		result = result.plus(fuel.times(TAX_RATE).minus(FUEL_TAX_CAP)).max(Dollars.ZERO);
 		return result;
