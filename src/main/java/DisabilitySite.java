e3880f4553b5d8ef1c44dabb650b9c88d916eec0
Victor Guerra Veloso <victorgvbh@gmail.com>
2023-02-20 12:15:48 -0500

    Reuse ResidentialSite.getDollars from DisabilitySite.charge (#1)

--- src/main/java/DisabilitySite.java
+++ src/main/java/DisabilitySite.java
@@ -31,24 +31,7 @@ public class DisabilitySite {
 		Dollars result;
 		double summerFraction;
 		int usage = Math.min(fullUsage, CAP);
-		if (start.after(_zone.summerEnd()) || end.before(_zone.summerStart()))
-			summerFraction = 0;
-		else if (!start.before(_zone.summerStart()) && !start.after(_zone.summerEnd()) &&
-				!end.before(_zone.summerStart()) && !end.after(_zone.summerEnd()))
-			summerFraction = 1;
-		else {
-			double summerDays;
-			if (start.before(_zone.summerStart()) || start.after(_zone.summerEnd())) {
-				// end is in the summer
-				summerDays = dayOfYear(end) - dayOfYear (_zone.summerStart()) + 1;
-			} else {
-				// start is in summer
-				summerDays = dayOfYear(_zone.summerEnd()) - dayOfYear (start) + 1;
-			}
-			summerFraction = summerDays / (dayOfYear(end) - dayOfYear(start) + 1);
-		}
-		result = new Dollars ((usage * _zone.summerRate() * summerFraction) +
-				(usage * _zone.winterRate() * (1 - summerFraction)));
+		result = ResidentialSite.getDollars(usage, start, end, _zone);
 		result = result.plus(new Dollars (Math.max(fullUsage - usage, 0) * 0.062));
 		result = result.plus(result.times(TAX_RATE));
 		Dollars fuel = new Dollars(fullUsage * 0.0175);
