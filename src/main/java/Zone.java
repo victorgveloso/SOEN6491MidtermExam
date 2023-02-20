3d773be53a4521f0da695b5b4f473318f0caebe4
Victor Guerra Veloso <victorgvbh@gmail.com>
2023-02-20 12:56:13 -0500

    Extract isLeapYear (#3)

--- src/main/java/Zone.java
+++ src/main/java/Zone.java
@@ -122,11 +122,14 @@ public class Zone {
 				throw new IllegalArgumentException();
 		}
 		result += arg.getDate();
-		//check leap year
-		if ((arg.getYear() % 4 == 0) && ((arg.getYear() % 100 != 0) ||
-				((arg.getYear() + 1900) % 400 == 0))) {
+		if (isLeapYear(arg)) {
 			result++;
 		}
 		return result;
+	}
+
+	private static boolean isLeapYear(Date arg) {
+		return (arg.getYear() % 4 == 0) && ((arg.getYear() % 100 != 0) ||
+				((arg.getYear() + 1900) % 400 == 0));
 	}
 }
